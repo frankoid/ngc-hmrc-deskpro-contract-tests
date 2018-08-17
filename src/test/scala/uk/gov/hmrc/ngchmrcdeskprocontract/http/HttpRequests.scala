493a8f6c98e3fe96ce91136d6a429a351eaaf50f
ngoulongkam <ngoulongkam2015@gmail.com>
2018-08-17 14:34:12 +0100

    [NK] Added contract test for get-help-ticket and feedback deskpro endpoint

--- src/test/scala/uk/gov/hmrc/ngchmrcdeskprocontract/http/HttpRequests.scala
+++ src/test/scala/uk/gov/hmrc/ngchmrcdeskprocontract/http/HttpRequests.scala
@@ -4,70 +4,79 @@ import java.net.URL
 
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.WSClient
+import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.ngchmrcdeskprocontract.hmrcdeskpro.CreateTicketResponse
+import uk.gov.hmrc.ngchmrcdeskprocontract.support.authloginapi.AuthLoginApiConnector
 import uk.gov.hmrc.ngchmrcdeskprocontract.support.{ServicesConfig, TestWSHttp}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-case class HttpRequests (wsClient: WSClient, config: ServicesConfig = new ServicesConfig){
+case class HttpRequests(wsClient: WSClient, config: ServicesConfig = new ServicesConfig) {
 
   private val http: TestWSHttp = new TestWSHttp(wsClient)
+  private val auth: AuthLoginApiConnector = new AuthLoginApiConnector(config, wsClient)
   private val hmrcDeskproBaseUrl: URL = new URL(config.baseUrl("hmrc-deskpro"))
   private val createGetHelpTicketUrl: URL = new URL(hmrcDeskproBaseUrl, "/deskpro/get-help-ticket")
   private val createFeedbackUrl: URL = new URL(hmrcDeskproBaseUrl, "/deskpro/feedback")
 
-  def createGetHelpTicket()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
+  def createGetHelpTicket()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[CreateTicketResponse] = {
     val createGetHelpTicketRequest = Json.parse(
       s"""
          |{
-         |  "name": ???,
-         |  "email": ???,
-         |  "subject": ???,
-         |  "message": ???,
-         |  "referrer": ???,
-         |  "javascriptEnabled": ???,
-         |  "userAgent": ???,
-         |  "authId": ???,
-         |  "areaOfTax": ???,
-         |  "sessionId": ???,
-         |  "userTaxIdentifiers": {
-         |    "nino": ???,
-         |    "ctUtr": ???,
-         |    "utr": ???,
-         |    "vrn": ???,
-         |    "empRef": ???
-         |  },
-         |  "service": ???
+         |  "name": "TestName",
+         |  "email": "testName@test.com",
+         |  "subject": "Test Subject",
+         |  "message": "Test Message",
+         |  "referrer": "test-journey-id",
+         |  "javascriptEnabled": "",
+         |  "userAgent": "TestAgent",
+         |  "authId": "",
+         |  "areaOfTax": "",
+         |  "sessionId": "",
+         |  "service": "HTS"
          |}
        """.stripMargin
     )
-    http.POST[JsValue, HttpRequests](createGetHelpTicketUrl.toString, createGetHelpTicketRequest).map { _ => () }
+    http.POST[JsValue, CreateTicketResponse](createGetHelpTicketUrl.toString, createGetHelpTicketRequest)
   }
 
-  def createFeedback()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
+  def createFeedback(nino: Nino)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[CreateTicketResponse] = {
     val createFeedbackRequest = Json.parse(
       s"""{
-        |  "name": ???,
-        |  "email": ???,
-        |  "subject": ???,
-        |  "message": ???,
-        |  "referrer": ???,
-        |  "javascriptEnabled": ???,
-        |  "userAgent": ???,
-        |  "authId": ???,
-        |  "areaOfTax": ???,
-        |  "sessionId": ???,
-        |  "userTaxIdentifiers": {
-        |    "nino": ???,
-        |    "ctUtr": ???,
-        |    "utr": ???,
-        |    "vrn": ???,
-        |    "empRef": ???
-        |  }
-        |  "rating": ???
-        |}
+         |  "name": "TestName",
+         |  "email": "testName@test.com",
+         |  "subject": "Test Subject",
+         |  "message": "Test Message",
+         |  "referrer": "test-journey-id",
+         |  "javascriptEnabled": "",
+         |  "userAgent": "TestAgent",
+         |  "authId": "",
+         |  "areaOfTax": "",
+         |  "sessionId": "",
+         |  "userTaxIdentifiers": {
+         |    "nino": "${nino.value}"
+         |  },
+         |  "rating": "5"
+         |}
       """.stripMargin
     )
-    http.POST[JsValue, HttpRequests](createFeedbackUrl.toString, createFeedbackRequest).map { _ => () }
+    http.POST[JsValue, CreateTicketResponse](createFeedbackUrl.toString, createFeedbackRequest)
+  }
+
+  def createInvalidFeedback()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[CreateTicketResponse] = {
+    val createFeedbackRequest = Json.parse("""{}""")
+
+    http.POST[JsValue, CreateTicketResponse](createFeedbackUrl.toString, createFeedbackRequest)
+  }
+
+  def createInvalidGetHelpTicket()(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[CreateTicketResponse] = {
+    val createGetHelpTicketRequest = Json.parse("""{}""")
+
+    http.POST[JsValue, CreateTicketResponse](createGetHelpTicketUrl.toString, createGetHelpTicketRequest)
+  }
+
+  def governmentGatewayLoginFor(nino: Nino)(implicit rec: ExecutionContext): Future[HeaderCarrier] = {
+    auth.governmentGatewayLogin(Some(nino))
   }
 }
